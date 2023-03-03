6d144d9d0ae8128e2755633ae704d265edf11132
seonghwan-park <125425707+seonghwan-park@users.noreply.github.com>
2023-03-03 11:33:53 +0900

    Update index.js
    시드니 시혀니

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ const IndexPage = () => {
       {/* Message Container */}
       <div className={styles.messageContainer}>
         <p>
-          This is a demonstration of the Sydney theme for verse by{' '}
+          This is a demonstration of the Sㅅydney theme for verse by{' '}
           <span className={styles.gold}>matter design.</span>
         </p>
         <p>
