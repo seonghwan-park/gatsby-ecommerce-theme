ac7ef1957b3c8eb2ef60eed71e3cd0dffd4af516
seonghwan-park <125425707+seonghwan-park@users.noreply.github.com>
2023-04-08 13:55:32 +0900

    message

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,7 +27,7 @@ const Header = (prop) => {
   const [search, setSearch] = useState('');
 
   const searchRef = createRef();
-  const bannerMessage = 'Free shipping worldwide';
+  const bannerMessage = '남들의 숨겨진 속마음을 알고싶어…';
   const searchSuggestions = [
     'Oversize sweaters',
     'Lama Pajamas',
